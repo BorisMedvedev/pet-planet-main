1753fc41060a31af27bccf25c29d2543e71deda0
John Doe <johndoe@example.com>
2024-06-04 09:30:59 +0300

    fix

--- src/js/fetchProducts.js
+++ src/js/fetchProducts.js
@@ -1,9 +1,13 @@
 import {renderProducts} from '/js/renderProducts';
-
 import {API_URL} from '../script';
+
+const preloader = document.querySelector('.preloader');
 
 export const fetchProducts = async (category) => {
   try {
+    // Показываем прелоадер перед началом запроса
+    preloader.classList.add('active');
+
     const response = await fetch(
         `${API_URL}/api/products/category/${category}`,
     );
@@ -16,10 +20,15 @@ export const fetchProducts = async (category) => {
     renderProducts(products);
   } catch (error) {
     console.error('Произошла ошибка при выполнении запроса:', error.message);
+  } finally {
+    // Скрываем прелоадер после выполнения запроса
+    setTimeout(() => {
+      preloader.classList.remove('active');
+    }, 2000);
   }
 };
-fetchProducts(
-    document.querySelector('.products__nav-btn--active').textContent,
-);
 
+// Получаем текущую активную категорию и выполняем запрос
+const activeCategory = document.querySelector('.products__nav-btn--active').textContent;
+fetchProducts(activeCategory);
 
